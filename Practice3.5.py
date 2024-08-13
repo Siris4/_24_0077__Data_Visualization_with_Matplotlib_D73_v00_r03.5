485cc431098ce68cfd4bf7d254f21dc5626c3a47
Gavin Martin <97780529+Siris4@users.noreply.github.com>
2024-08-12 21:03:52 -0600

    Add files via upload

---
+++ Practice3.5.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import os
+
+# Change the working directory
+os.chdir('C:\\Users\\Siris\\Desktop\\GitHub Projects 100 Days NewB\\_24_0077__Day73_Data_Visualization_with_Matplotlib__240812\\NewProject\\r00-r09 START\\r00_env_START')
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv('QueryResults.csv', names=['DATE', 'TAG', 'POSTS'], header=0)
+
+print(df.shape)
+
+# Verify the changes
+print(df.head())
+
+# 1991 rows, 3 columns
